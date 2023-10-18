efd6f8ee2fe3418322ff7b2032a90bf829da9773
TusharShandilya <tusharshandilya@live.com>
2023-10-18 19:52:31 +0530

    feat(Location): No data/welcome page added

---
+++ src/components/features/Location/Routes/LocationsWelcome.tsx
@@ -0,0 +1,14 @@
+const LocationsWelcome = () => {
+  return (
+    <div className="mt-8 flex flex-col ">
+      <h1 className="text-4xl font-bold text-center text-gray-800 mb-2">
+        Welcome
+      </h1>
+      <h2 className="text-lg font-bold text-center text-gray-600">
+        Please select a location 📍{" "}
+      </h2>
+    </div>
+  );
+};
+
+export default LocationsWelcome;
--- src/components/features/Location/Routes/index.tsx
+++ src/components/features/Location/Routes/index.tsx
@@ -1,12 +1,18 @@
+import { RouteObject } from "react-router-dom";
 import LocationById from "./LocationById";
 import LocationEdit from "./LocationEdit";
 import Locations from "./Locations";
+import LocationsWelcome from "./LocationsWelcome";
 
 // QUERY: Assuming locations page will be the home page (can be changed to "/locations")
-const locationRoutes = {
+const locationRoutes: RouteObject = {
   path: "/",
   element: <Locations />,
   children: [
+    {
+      index: true,
+      element: <LocationsWelcome />,
+    },
     {
       path: ":locid",
       element: <LocationById />,
