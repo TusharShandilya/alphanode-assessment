b3aa6cdbea3fe185a07717a9e1e2926c1f7fd09e
TusharShandilya <tusharshandilya@live.com>
2023-10-18 20:32:24 +0530

    feat(Location): location by id api

--- src/components/features/Location/Routes/LocationById.tsx
+++ src/components/features/Location/Routes/LocationById.tsx
@@ -1,9 +1,32 @@
-import { Outlet } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { Outlet, useParams } from "react-router-dom";
+import { GET_LOCATION_BY_ID } from "../api/Location.queries";
+import { LocationReadQuery } from "../../../../types/gql/graphql";
+import config from "../../../../config";
+import Error from "../../../common/molecules/Error";
+import Loading from "../../../common/molecules/Loading";
 
 const LocationById = () => {
+  const { locid } = useParams();
+
+  const location = useQuery<LocationReadQuery>(GET_LOCATION_BY_ID, {
+    variables: {
+      tenant: config.temp_vars.tenant,
+      locationReadId: locid,
+    },
+  });
+
+  if (location.loading) {
+    return <Loading />;
+  }
+
+  if (location.error) {
+    return <Error />;
+  }
+
   return (
     <div>
-      LocationById Page <Outlet />
+      LocationById Page {locid} <Outlet />
     </div>
   );
 };
